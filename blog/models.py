c22c3d219b126b71951377285b18ce1b438a1b1d
Husain Ebrahim <husain@alsayed.ws>
2018-06-18 00:18:50 +0300

    final version

--- blog/models.py
+++ blog/models.py
@@ -5,4 +5,16 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateField()
     body = models.TextField()
-    image = models.ImageField(name='images')
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def date_short(self):
+        return self.pub_date.strftime('%b %Y')
+
+    def summary(self):
+        summary = self.body[:300]
+        summary = summary[:summary.rfind(' ')]
+        return summary
+
